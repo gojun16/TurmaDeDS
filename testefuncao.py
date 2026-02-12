fa625a087499e21140e91b4eae78e6d13790f41c
gojun16 <fernando.schlichting@escola.pr.gov.br>
2026-02-12 08:36:55 -0300

    Create testefuncao.py

---
+++ testefuncao.py
@@ -0,0 +1,11 @@
+# Soma com uso de função personalizada
+
+def somar(a, b):
+
+   return(a + b)
+
+a = int(input('Digite o número 1: ')) #entrada 1 pelo usuário 
+
+b = int(input('Digite o número 2: ')) #entrada 2 pelo usuário
+print(f'A soma de {a} e {b} é {somar(a,b)}')
+
