e73fb2dd0ddd25310b941ce014f2b6458627731a
gojun16 <fernando.schlichting@escola.pr.gov.br>
2026-02-12 08:31:39 -0300

    Add initial test script for sum operations

---
+++ testesiniciais.py
@@ -0,0 +1,14 @@
+# 1 - soma e impressão do resultado
+a = 458
+b = 789
+t = a + b
+print(f"A soma é: {t}\n")
+# 2- soma com a função embutida sum()
+numeros = [4, 5, 6, 7, 8]
+resultado = sum(numeros)
+print(resultado)
+print()
+# 3- soma com uso de variável
+soma = 10 + 5 + 2
+print(soma)
+print()
