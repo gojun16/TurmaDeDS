3eb750d2decea283cef379f7fcaa98a1b93bc3dd
gojun16 <fernando.schlichting@escola.pr.gov.br>
2026-04-06 08:38:36 -0300

    Update sistema-livraria.sql

--- bancoDeDados/sistema-livraria.sql
+++ bancoDeDados/sistema-livraria.sql
@@ -14,3 +14,18 @@ CREATE TABLE Compras (
     NomeLivro VARCHAR(255),
     FOREIGN KEY (ClienteID) REFERENCES Clientes(ID)
 );
+
+-- Inserindo dados na tabela Clientes
+INSERT INTO Clientes (ID, nomeCliente, emailCliente) VALUES 
+(1, 'Maria Silva', 'maria.silva@email.com'),
+(2, 'João Souza', 'joao.souza@email.com'),
+(3, 'Ana Oliveira', 'ana.oliveira@email.com');
+
+-- Inserindo dados na tabela Compras
+INSERT INTO Compras (CompraID, ClienteID, NomeLivro) VALUES 
+(101, 1, 'Quarto de Despejo'),
+(102, 2, 'Dom Casmurro'),
+(103, 3, 'Quarto de Despejo'),
+(104, 1, 'A Hora da Estrela');
+--
+SELECT * FROM Clientes;
